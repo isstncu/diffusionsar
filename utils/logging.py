8dbeacb9350d1a839c0a1e5ca0f7fb60db8097b3
Jingbo.WEI <kongniao@hotmail.com>
2023-12-13 15:11:43 +0800

    Add files via upload

---
+++ utils/logging.py
@@ -0,0 +1,32 @@
+import imageio
+import numpy
+import numpy as np
+import torch
+import os
+
+
+def save_image(img, file_directory):
+    if not os.path.exists(os.path.dirname(file_directory)):
+        os.makedirs(os.path.dirname(file_directory))
+    # tvu.save_image(img, file_directory)
+    # sar图像存储
+    img = torch.clamp(img, 0.0, 1.0)
+    # print(img.shape)
+    img = img.reshape(img.shape[-2], img.shape[-1])
+    img = img.squeeze().cpu().numpy()
+    img = (img*255).astype(numpy.uint8)
+    # img = denormalize_sar(img) # 指数变换转回SAR数据形式存储
+    imageio.v3.imwrite(file_directory, img)
+
+
+def save_checkpoint(state, filename):
+    if not os.path.exists(os.path.dirname(filename)):
+        os.makedirs(os.path.dirname(filename))
+    torch.save(state, filename + '.pth.tar')
+
+
+def load_checkpoint(path, device):
+    if device is None:
+        return torch.load(path)
+    else:
+        return torch.load(path, map_location=device)
