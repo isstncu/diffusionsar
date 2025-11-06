0058a0992be5fca69fa2b8fdabd7842e17faa01f
Jingbo.WEI <kongniao@hotmail.com>
2025-11-06 11:34:39 +0100

    Update logging.py

--- utils/logging.py
+++ utils/logging.py
@@ -4,6 +4,8 @@ import numpy as np
 import torch
 import os
 
+import utils
+
 
 def save_image(img, file_directory):
     if not os.path.exists(os.path.dirname(file_directory)):
@@ -14,6 +16,15 @@ def save_image(img, file_directory):
     img = (img*255).astype(numpy.uint8)
     imageio.v3.imwrite(file_directory, img)
 
+def save_image_v2(img, lambda_, file_directory):
+    if not os.path.exists(os.path.dirname(file_directory)):
+        os.makedirs(os.path.dirname(file_directory))
+    img = torch.clamp(img, 0.0, 1.0)
+    # print(img.shape)
+    img = img.reshape(img.shape[-2], img.shape[-1])
+    img = img.squeeze().cpu().numpy()
+    img = utils.inverse_x0_process_v3(img,lambda_) #inverse_x0_process_v2 or inverse_x0_process_v3
+    imageio.v3.imwrite(file_directory, img)
 
 def save_checkpoint(state, filename):
     if not os.path.exists(os.path.dirname(filename)):
