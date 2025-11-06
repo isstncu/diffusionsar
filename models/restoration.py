73a25b2927095e8f768ba8cb6127af64ae656da3
Jingbo.WEI <kongniao@hotmail.com>
2025-11-06 11:29:59 +0100

    Refactor DiffusiveRestoration class methods
    Updated image saving path and modified restore method to include additional input.

--- models/restoration.py
+++ models/restoration.py
@@ -26,23 +26,25 @@ class DiffusiveRestoration:
     def restore(self, val_loader, r=None):
         validation = self.config.training.name
         # 单张图
-        image_folder = "/home/jbwei/Pke/test_syn_sar/output"
+        image_folder = os.path.join('./result','ckpts', self.config.training.version, 'test') 
         with torch.no_grad():
             for i, (x, y) in enumerate(val_loader):
                 y = y[-1]
                 print(f"starting processing from image {y}")
                 x = x.flatten(start_dim=0, end_dim=1) if x.ndim == 5 else x
                 x_cond = x[:, :1, :, :].to(self.diffusion.device)
-                print(x_cond.shape)
-                x_output = self.diffusive_restoration(x_cond, r=r)
-                #x_output = inverse_data_transform(x_output) # torch.Size([1,1,img_size,img_size])
-                utils.logging.save_image(x_output, os.path.join(image_folder, f"{y}_ddpm_L1_output.tif")) # png->tif
-
-    def diffusive_restoration(self, x_cond, r=None):
+                xt = x[:, 1:, :, :].to(self.diffusion.device)
+                #x_output = self.diffusive_restoration(x_cond, r=r)
+                x_output = self.diffusive_restoration(x_cond, xt, r=r)
+                # x_output = inverse_data_transform(x_output) # torch.Size([1,1,img_size,img_size])
+                utils.logging.save_image_v2(x_output, lambda_, os.path.join(image_folder, f"{y}_ddpm_best.tif"))
+                #utils.logging.save_image(x_output, os.path.join(image_folder, f"{y}_ddpm_best.tif"))
+    
+    def diffusive_restoration(self, x_cond, x, r=None):
         p_size = self.config.data.image_size
         h_list, w_list = self.overlapping_grid_indices(x_cond, output_size=p_size, r=r)
         corners = [(i, j) for i in h_list for j in w_list]
-        x = torch.randn(x_cond.size(), device=self.diffusion.device)
+        #x = torch.randn(x_cond.size(), device=self.diffusion.device)
         x_output = self.diffusion.sample_image(x_cond, x, patch_locs=corners, patch_size=p_size)
         return x_output
 
@@ -52,3 +54,4 @@ class DiffusiveRestoration:
         h_list = [i for i in range(0, h - output_size + 1, r)]
         w_list = [i for i in range(0, w - output_size + 1, r)]
         return h_list, w_list
+
