7f6a499525fc9dc630bff97d5cebcda984d85a07
Jingbo.WEI <kongniao@hotmail.com>
2026-08-09 16:49:20 +0200

    Update ddm.py

--- models/ddm.py
+++ models/ddm.py
@@ -75,7 +75,8 @@ def get_beta_schedule(beta_schedule, *, beta_start, beta_end, num_diffusion_time
     if beta_schedule == "quad":
         betas = (np.linspace(beta_start ** 0.5, beta_end ** 0.5, num_diffusion_timesteps, dtype=np.float64) ** 2)
     elif beta_schedule == "linear":
-        betas = np.linspace(beta_start, beta_end, num_diffusion_timesteps, dtype=np.float64)
+        #betas = np.linspace(beta_start, beta_end, num_diffusion_timesteps, dtype=np.float64)
+        betas = np.linspace(beta_start, beta_end, 1000, dtype=np.float64)
     elif beta_schedule == "const":
         betas = beta_end * np.ones(num_diffusion_timesteps, dtype=np.float64)
     elif beta_schedule == "jsd":  # 1/T, 1/(T-1), 1/(T-2), ..., 1
@@ -85,6 +86,7 @@ def get_beta_schedule(beta_schedule, *, beta_start, beta_end, num_diffusion_time
         betas = sigmoid(betas) * (beta_end - beta_start) + beta_start
     else:
         raise NotImplementedError(beta_schedule)
+    betas=betas[:num_diffusion_timesteps]
     assert betas.shape == (num_diffusion_timesteps,)
     return betas
 
