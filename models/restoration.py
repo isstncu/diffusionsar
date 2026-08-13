7901735995ea4b5d25c28a13980c2cba439edb7a
Jingbo.WEI <kongniao@hotmail.com>
2026-08-13 20:23:43 +0200

    Update restoration.py

--- models/restoration.py
+++ models/restoration.py
@@ -2,10 +2,6 @@ import torch
 import utils
 import os
 
-# def inverse_data_transform(X):
-#     #return torch.clamp((X + 1.0) / 2.0, 0.0, 1.0)
-#     return (X + 1.0) / 2.0
-
 class DiffusiveRestoration:
     def __init__(self, diffusion, args, config):
         super(DiffusiveRestoration, self).__init__()
@@ -19,35 +15,45 @@ class DiffusiveRestoration:
         else:
             print('Pre-trained diffusion model path is missing!')
 
-    def restore(self, val_loader, r=None):
-        validation = self.config.training.name
-        image_folder = os.path.join('./result','ckpts', self.config.training.version, 'test') 
+    def restore(self, test_loader, r=None):
+        image_folder = os.path.join(self.args.image_folder)
         with torch.no_grad():
-            for i, (x, y, lambda_) in enumerate(val_loader):
-                y = y[-1]
+            #for i, (xx,filename) in enumerate(test_loader):#gaussian noise
+            for i, (xx,filename, lambda_) in enumerate(test_loader):#gamma noise
+                y = filename[0].split("'")[0]
                 print(f"starting processing from image {y}")
+
+                x=xx[0]
                 x = x.flatten(start_dim=0, end_dim=1) if x.ndim == 5 else x
-                x_cond = x[:, :1, :, :].to(self.diffusion.device)
-                xt = x[:, 1:, :, :].to(self.diffusion.device)
-                #x_output = self.diffusive_restoration(x_cond, r=r)
-                x_output = self.diffusive_restoration(x_cond, xt, r=r)
-                # x_output = inverse_data_transform(x_output) # torch.Size([1,1,img_size,img_size])
-                utils.logging.save_image_v2(x_output, lambda_, os.path.join(image_folder, f"{y}_ddpm_best.tif"))
-                #utils.logging.save_image(x_output, os.path.join(image_folder, f"{y}_ddpm_best.tif"))
+                x_cond = xx[0].to(self.diffusion.device)
+                xt = xx[1].to(self.diffusion.device)
+                
+                x_output = self.diffusive_restoration(x_cond, x=xt, r=r)
+
+                #utils.logging.save_image(x_output, os.path.join(image_folder, f"PRED_{y}.tif"))#gaussian noise
+                utils.logging.save_image_v2(x_output, lambda_, os.path.join(image_folder, f"PRED_{y}.tif"))#gamma noise
     
-    def diffusive_restoration(self, x_cond, x, r=None):
+    def diffusive_restoration(self, x_cond, x=None, r=None):
         p_size = self.config.data.image_size
         h_list, w_list = self.overlapping_grid_indices(x_cond, output_size=p_size, r=r)
         corners = [(i, j) for i in h_list for j in w_list]
-        #x = torch.randn(x_cond.size(), device=self.diffusion.device)
+        
+        #x = torch.randn(x_cond.size(), device=self.diffusion.device)#gaussian noise
         x_output = self.diffusion.sample_image(x_cond, x, patch_locs=corners, patch_size=p_size)
         return x_output
 
     def overlapping_grid_indices(self, x_cond, output_size, r=None):
-        _, c, h, w = x_cond.shape
+        _, _, h, w = x_cond.shape
         r = 16 if r is None else r
-        h_list = [i for i in range(0, h - output_size + 1, r)]
-        w_list = [i for i in range(0, w - output_size + 1, r)]
+
+        h_list = list(range(0, h - output_size + 1, r))
+        w_list = list(range(0, w - output_size + 1, r))
+
+        if h_list[-1] != h - output_size:
+            h_list.append(h - output_size)
+
+        if w_list[-1] != w - output_size:
+            w_list.append(w - output_size)
+
         return h_list, w_list
-
-
+        
