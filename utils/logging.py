77134cb4c905169aacbeb3fb80e7f2e8b5d20688
Jingbo.WEI <kongniao@hotmail.com>
2026-08-13 20:27:26 +0200

    Update logging.py

--- utils/logging.py
+++ utils/logging.py
@@ -3,34 +3,30 @@ import numpy
 import numpy as np
 import torch
 import os
-
 import utils
-
 
 def save_image(img, file_directory):
     if not os.path.exists(os.path.dirname(file_directory)):
         os.makedirs(os.path.dirname(file_directory))
+    
     img = torch.clamp(img, 0.0, 1.0)
-    img = img.reshape(img.shape[-2], img.shape[-1])
     img = img.squeeze().cpu().numpy()
-    img = (img*255).astype(numpy.uint8)
+    #img = (img*255).astype(numpy.uint8)#if 0-255 else 0-1
     imageio.v3.imwrite(file_directory, img)
 
 def save_image_v2(img, lambda_, file_directory):
     if not os.path.exists(os.path.dirname(file_directory)):
         os.makedirs(os.path.dirname(file_directory))
-    img = torch.clamp(img, 0.0, 1.0)
-    # print(img.shape)
-    img = img.reshape(img.shape[-2], img.shape[-1])
+    
+    #img = torch.clamp(img, 0.0, 1.0)
     img = img.squeeze().cpu().numpy()
-    img = utils.inverse_x0_process_v3(img,lambda_) #inverse_x0_process_v2 or inverse_x0_process_v3
+    img = utils.inverse_x0_process_v3(img,lambda_)
     imageio.v3.imwrite(file_directory, img)
 
 def save_checkpoint(state, filename):
     if not os.path.exists(os.path.dirname(filename)):
         os.makedirs(os.path.dirname(filename))
     torch.save(state, filename + '.pth.tar')
-
 
 def load_checkpoint(path, device):
     if device is None:
